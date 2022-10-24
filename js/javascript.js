a6964f8ceda16cf397a6ae5641d6b44b1ed46a7c
hung <a0903131602@gmail.com>
2022-10-24 23:58:18 +0800

    reset2

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+const toggleBtn = document.querySelector('#toggleBtn')
+const body = document.querySelector('body')
+
+// toggleBtn.addEventListener('click',(e)=>{
+//     e.preventDefault()
+//     body.classList.toggle('sidebar-toggle')
+// })
+
+
+console.log(toggleBtn)
